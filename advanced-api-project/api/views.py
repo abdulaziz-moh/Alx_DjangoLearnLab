465ce6740ca89f56549c089e04d44360d680be8d
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-10 22:02:20 +0300

    Missing Filter/Ordering/Search Imports for the BookDetailView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,10 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 
 # Create your views here.
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions,filters
+from rest_framework import generics, permissions
+from django_filters import rest_framework as filters
+from rest_framework import filters as drf_filters
+
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,6 +15,15 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  # Read-only for unauthenticated users
+
+    filter_backends = [
+        filters.DjangoFilterBackend,
+        drf_filters.SearchFilter,
+        drf_filters.OrderingFilter
+    ]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 # class BookListView(generics.ListAPIView):
 #     queryset = Book.objects.all()
