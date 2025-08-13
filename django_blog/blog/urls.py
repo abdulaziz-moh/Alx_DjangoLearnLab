208a3d4de7846b23b7dac15269524161541cbcd5
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-14 01:14:14 +0300

    created the templates for the basic functionalities(like login,logout,register,profie) and corrected an error in customizing the usercreationform

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/',views.login_view, name = 'login'),
+    path('logout/',views.logout_view, name = 'logout'),
+    path('profile/',views.profile_view, name = 'profile'),
+    path('register/',views.register_view, name = 'register'),
+    path('posts/',views.base_view, name = 'posts'),
+    path('',views.base_view, name = 'home'),
+    
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,38 +5,43 @@ from django.contrib.auth.decorators import login_required
 from . forms import SignupForm
 # Create your views here.
 
-def signup_view(request):
+def register_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save() # we may want it for automatic login
-            
-            return redirect('login')
+
+            return redirect('login') # because render will not change the url bar, even after you enter login credentials it will return you to register url(again)
         else:
-            return render(request,'signup.html',{'form':form})
+            return render(request,'registration.html',{'form':form})
     else:
         form = SignupForm()
-    return render(request,'signup.html',{'form':form})
+    return render(request,'registration.html',{'form':form})
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(data = request.POST)
 
         if form.is_valid():
             user = form.get_user()
             login(request,user)
+            
+            return render(request,"base.html")
         else:
             return render(request,'login.html',{'form':form})
         
     else:
         form = AuthenticationForm()
-    return render(request,'login .html',{'form':form})
+    return render(request,'login.html',{'form':form})
 
 def logout_view(request):
     logout(request)
-    return render(request,'loggedoutuser.html')
+    return render(request,'logout.html')
     
-@login_required
-def manageprofile_view(request):
-    return redirect('profile')
+# @login_required
+def profile_view(request):
+    return render(request,'profile.html')
+
+def base_view(request):
+    return render(request,'base.html')
