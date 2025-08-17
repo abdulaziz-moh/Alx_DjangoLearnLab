7f255592d74d6be5a570f81b36a1cecaed7995bf
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-17 09:00:22 +0300

    correcting the urs.py for the grader

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,29 +3,28 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name = 'login'),
-    path('logout/',auth_views.LogoutView.as_view(next_page='home'), name = 'logout'),
-    path('register/',views.register, name = 'register'),
-    path('profile/',views.profile, name = 'profile'),
-    
-    
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
     # Home
     path('', views.PostListView.as_view(), name='home'),
 
     # Post CRUD
-    path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+
+    # Comment CRUD
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 
-    path('tag/<int:tag_id>/', views.posts_by_tag, name='posts-by-tag'),
-    # path('search/', views.search_posts, name='search-posts'),
+    # Tags
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+
+    # Search
     path('search/', views.SearchResultsView.as_view(), name='search-results'),
-
-
 ]
