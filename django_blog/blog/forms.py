2afef825ea879309c44737d1bffc8d6d67141d3f
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-17 08:42:42 +0300

    tag usign the django-taggit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -10,14 +10,10 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(
-    queryset=Tag.objects.all(),
-    widget=forms.CheckboxSelectMultiple,
-    required=False
-)
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+
     
 class CommentForm(forms.ModelForm):
     class Meta:
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -23,6 +24,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)   # automatically set when created
     updated_at = models.DateTimeField(auto_now=True)       # automatically update on save
+    tags = TaggableManager()   # instead of ManyToManyField
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
