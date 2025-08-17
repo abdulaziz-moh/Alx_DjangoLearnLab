2c31e831bdf76be40f3a544adc10456d98ec3c5a
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-08-17 08:35:17 +0300

    check for mpdel updation f

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -21,8 +21,8 @@ class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)   # automatically set when created
+    updated_at = models.DateTimeField(auto_now=True)       # automatically update on save
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
